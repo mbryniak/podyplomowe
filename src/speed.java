d777ab44f5ad2b0f918aa11563da2405513f235e
mmbryniak <mbryniak@student.wszib.edu.pl>
2023-11-02 23:57:48 +0100

    Initial commit

---
+++ src/speed.java
@@ -0,0 +1,7 @@
+public class speed {
+    public static void main(String[] args) {
+        int t = 60;
+        int s = 3000;
+        System.out.println("Średnia prędkość wynosi " + s/t + " km/h ");
+    }
+}
